45b85cdb72f71aa56f8d1cbbfee7acb6f08fbee3
Kevin <kevinquillen@users.noreply.github.com>
2023-07-31 10:03:04 -0400

    Update ViewExtractorTest.php

--- tests/src/Unit/Utility/ViewExtractorTest.php
+++ tests/src/Unit/Utility/ViewExtractorTest.php
@@ -6,7 +6,7 @@ use Drupal\mymodule\Utility\ViewExtractor;
 use Drupal\Tests\UnitTestCase;
 
 /**
- * Unit test for ViewExtractorTest class.
+ * Unit test for ViewExtractor class.
  *
  * @group mymodule
  */
